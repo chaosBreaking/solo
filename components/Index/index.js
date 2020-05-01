307fb88dbf75f8a3c6b30621341429d1652de971
chaosBreaking <x491807573@qq.com>
2020-05-02 00:52:53 +0800

    feat: random background image

--- components/Index/index.js
+++ components/Index/index.js
@@ -6,6 +6,12 @@ import withStyles from 'isomorphic-style-loader/withStyles';
 import AuthCard from './components/AuthCard';
 import s from './index.scss';
 
+const BG_IMAGES = [
+    'http://solo-app.test.upcdn.net/home_bg-13mk9v-progressive.jpeg',
+    'http://solo-app.test.upcdn.net/home_bg_xj3haef-progressive.jpeg',
+    'http://solo-app.test.upcdn.net/wallhaven-96w8e8.jpeg'
+];
+
 @withStyles(s)
 @pageWrapper({
     Store
@@ -17,9 +23,9 @@ export default class Home extends Component {
         return (
             <div className={s.container}>
                 <div className={s.banner}>
-                    <span className={s.bottomInfo}>随便逛逛?</span>
+                    <span className={s.bottomInfo}>Solo是什么?</span>
                 </div>
-                <div className={s.home}>
+                <div className={s.home} style={{ backgroundImage: `url(${BG_IMAGES[~~(Math.random() * 10) % 3]})` }}>
                     <AuthCard />
                 </div>
             </div>
