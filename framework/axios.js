9c483fa358af4721a846fbc59092e1b37533ac6f
chaosBreaking <x491807573@qq.com>
2021-04-13 15:00:14 +0800

    fix: api地址

--- framework/axios.js
+++ framework/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import enhanceRequest from './enhanceRequest';
 
 enhanceRequest(axios);
-axios.defaults.baseURL = __DEV__ ? 'http://localhost:6057/api/v1' : 'http://api.hyperii.com/api/v1';
+axios.defaults.baseURL = __DEV__ ? 'http://localhost:6057/api/v1' : 'http://api.hyperii.com/solo/api/v1';
 
 export default axios;
